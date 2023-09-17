791217c850cd945210aa89b83d0d3c4cc07618d7
Desmond Chen <desmondddd@icloud.com>
2023-09-17 17:33:35 +0800

    Removed unused import in Aggregate.kt

--- src/main/kotlin/com/example/demo/core/Aggregate.kt
+++ src/main/kotlin/com/example/demo/core/Aggregate.kt
@@ -1,8 +1,6 @@
 package com.example.demo.core
 
-import org.springframework.data.annotation.Version
-
-interface Aggregate: Entity {
+interface Aggregate : Entity {
     override val id: String
     override val version: Long?
 }
