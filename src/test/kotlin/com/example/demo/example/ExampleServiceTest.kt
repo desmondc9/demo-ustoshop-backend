c74f5b521c6917f8eb34f5081a8de63bb48c5f16
Desmond Chen <desmondddd@icloud.com>
2023-09-17 11:44:29 +0800

    Deleted example package

--- src/test/kotlin/com/example/demo/example/ExampleServiceTest.kt
+++
@@ -1,7 +0,0 @@
-package com.example.demo.example
-
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class ExampleServiceTest {
-}
